46d155cd7e85fcfcdd7a59246e3b1f7e378a288b
Natalie <nmarks413@gmail.com>
2024-11-06 17:36:57 -0800

    replace pyright with basedpyright

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,117 +1,116 @@
 return {
-  {
-    --downloads lsps
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    --integrates mason and lspconfig
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    opts = {
-      inlay_hints = {
-        enabled = true,
-      },
-    },
-    config = function()
-      --pull in nvim cmp deps
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
+	{
+		--downloads lsps
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		--integrates mason and lspconfig
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		opts = {
+			inlay_hints = {
+				enabled = true,
+			},
+		},
+		config = function()
+			--pull in nvim cmp deps
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-      local lspconfig = require("lspconfig")
-      -- lspconfig.tsserver.setup({
-      -- 	capabilites = capabilities,
-      -- })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      filetypes = { "lua" },
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          completion = {
-            callSnippet = "Replace",
-          },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          format = {
-            defaultConfig = {
-            },
-          },
-          hint = {
-            enable = true,
-          },
-        },
-    },
-      })
-      lspconfig.nil_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.texlab.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.typst_lsp.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.denols.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.hls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      -- lspconfig.jedi_language_server.setup({
-      -- 	capabilities = capabilities,
-      -- })
-      lspconfig.zls.setup({
-        capabilities = capabilities,
-      })
+			local lspconfig = require("lspconfig")
+			-- lspconfig.tsserver.setup({
+			-- 	capabilites = capabilities,
+			-- })
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				filetypes = { "lua" },
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						completion = {
+							callSnippet = "Replace",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						format = {
+							defaultConfig = {},
+						},
+						hint = {
+							enable = true,
+						},
+					},
+				},
+			})
+			lspconfig.nil_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.texlab.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.typst_lsp.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.denols.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.hls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.basedpyright.setup({
+				capabilities = capabilities,
+			})
+			-- lspconfig.jedi_language_server.setup({
+			-- 	capabilities = capabilities,
+			-- })
+			lspconfig.zls.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "<leader>th", function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-      end, { desc = "Toggle Inlay Hints" })
-    end,
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-    config = function()
-      local trouble = require("trouble")
-      vim.keymap.set("n", "<leader>xq", function()
-        trouble.toggle("quickfix")
-      end)
-      vim.keymap.set("n", "<leader>xx", function()
-        trouble.toggle("document_diagnostics")
-      end)
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>th", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+			end, { desc = "Toggle Inlay Hints" })
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		config = function()
+			local trouble = require("trouble")
+			vim.keymap.set("n", "<leader>xq", function()
+				trouble.toggle("quickfix")
+			end)
+			vim.keymap.set("n", "<leader>xx", function()
+				trouble.toggle("document_diagnostics")
+			end)
+		end,
+	},
 }
